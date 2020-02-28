5938a0684f99a684ad84d799a0de20847c34581c
Frank Zhou <529808348@qq.com>
2020-02-28 16:09:55 +0800

    Change test

--- rest/api_test.go
+++ rest/api_test.go
@@ -66,8 +66,8 @@ func TestByBit_CreateStopOrder(t *testing.T) {
 	orderType := "Limit"
 	qty := 30
 	price := 10000.0
-	basePrice := 10000.0
-	stopPx := 6000.0
+	basePrice := 10000.0 // 触发价
+	stopPx := 0.0
 	triggerBy := ""
 	timeInForce := "GoodTillCancel"
 	// {"ret_code":0,"ret_msg":"ok","ext_code":"","result":{"user_id":103061,"symbol":"BTCUSD","side":"Buy","order_type":"Limit","price":"7000","qty":30,"time_in_force":"GoodTillCancel","order_status":"Created","ext_fields":{"cross_status":"PendingNew","xreq_type":"x_create","xreq_offset":148672558},"leaves_qty":30,"leaves_value":"0.00428571","reject_reason":"","cross_seq":-1,"created_at":"2019-07-23T08:54:54.000Z","updated_at":"2019-07-23T08:54:54.000Z","last_exec_time":"0.000000","last_exec_price":0,"order_id":"603c41e0-c9fb-450c-90b6-ea870d5b0180"},"ext_info":null,"time_now":"1563872094.895918","rate_limit_status":98}
